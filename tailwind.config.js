8100f3545b3b939b8e14bbdb0e2f97cfe0f12c9e
Muhammad <muhammadtalha89@gmail.com>
2023-05-10 15:52:41 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Inter", ...fontFamily.sans],
+      },
+      colors: {
+        hm: {
+          orange: "#FE621D",
+          blue: "#71BBFF",
+          grayE1: "#CBD5E1",
+          black: "#080E1B",
+        },
+        neutral: {
+          500: "#737373",
+        },
+      },
+      backgroundImage: {
+        "gradient-text":
+          "linear-gradient(109.97deg, #FFFFFF -32.38%, rgba(0, 0, 0, 0) 55.95%)",
+        "gradient-dive":
+          "linear-gradient(180deg, #E5E5E5 0%, #83878F 43.23%, #080E1B 93.75%)",
+      },
+    },
+  },
+  plugins: [],
+};
